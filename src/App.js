cb8402ed48dc7888ef68806db3c6761418d89769
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-08-17 21:28:40 -0400

    layout created in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 // Import data
-
+import {postings} from './postings.js'
 // Import components
 import './App.css';
+import Gallery from './Gallery.js'
+import Directory from './Directory.js'
+import SearchBar from './SearchBar.js'
+import Sidebar from './Sidebar.js'
 
 function App(){
   return (
     <div>
       <h1>craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+        <Sidebar/>
+        <SearchBar/>
+        <Directory/>
+        <Gallery postings = {postings}/>
       </div>
     </div>
   );
---
+++ src/Gallery.js
@@ -0,0 +1,14 @@
+import Posting from "./Posting";
+
+function Gallery(props) {
+  return (
+    <div className="gallery">
+      {/* 4 use the .map to iterate the props posting array */}
+      {props.postings.map((p, i) => {
+        return <Posting posting={p} key={i} />;
+      })}
+    </div>
+  );
+}
+
+export default Gallery;
